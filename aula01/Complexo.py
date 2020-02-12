613d93ae3c2ac64e7eafbd095aeccb39632889d2
Ricardo Duarte Taveira <ricdtaveira@gmail.com>
2020-02-12 15:15:23 -0300

    12/02/20 15:15

--- aula01/Complexo.py
+++ aula01/Complexo.py
@@ -1,7 +1,22 @@
-class Complex:
-    def __init__(self, realpart, imagpart):
-        self.r = realpart
-        self.i = imagpart
+class Complexo:
+    def __init__(self):
+        self.r = 0.0
+        self.i = 0.0
         
-x = Complex(3.0, -4.5)
-print(x.r, x.i)
+    def set_i(self, i):
+        self.i = i
+        
+    def get_i(self):
+        return self.i
+    
+    def set_r(self, r):
+        self.r = r
+        
+    def get_r(self):
+        return self.r
+        
+x = Complexo()
+x.set_i(-2.0)
+x.set_r(5.0)
+
+print(x.get_r, x.get_i)
