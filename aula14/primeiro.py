f8d2e5baba50c104bf600a82a9c3821201e07533
Ricardo Taveira <ricdtaveira@gmail.com>
2020-08-19 13:14:40 -0300

    Atualizado em 19-08-2020

---
+++ aula14/primeiro.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+class Application:
+    def __init__(self, master=None):
+        self.widget1 = Frame(master)
+        self.widget1.pack()
+        self.msg = Label(self.widget1, text="Primeiro widget")
+        self.msg.pack ()
+
+
+root = Tk()
+Application(root)
