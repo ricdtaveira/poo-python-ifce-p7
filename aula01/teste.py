ed7411a87a7ba72bb1300c31a2ea17c7d2122419
Ricardo Taveira <ricdtaveira@gmail.com>
2021-04-20 14:15:20 -0300

    Alteração em 20-04-2021

--- aula01/teste.py
+++ aula01/teste.py
@@ -2,6 +2,12 @@
 from aluno import Aluno 
 
 a = Aluno(100, "Joana", "Av. 13 de Maio 2081")
+b = Aluno(200, "João Pedro", "Av. João Pessoa 1200")
 
+print(a)
+print(b)
+a.printSelf()
 print(a.toString())
+b.printSelf()
+print(b.toString())
 
