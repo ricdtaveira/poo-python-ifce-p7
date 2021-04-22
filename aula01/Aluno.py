81d695df5a56630dc040624b6d62287abffdff32
Ricardo Taveira <ricdtaveira@gmail.com>
2021-04-22 14:54:36 -0300

    Alteração em 22-04-2021

--- aula01/Aluno.py
+++ aula01/Aluno.py
@@ -1,11 +1,16 @@
 class Aluno:
-     
+
+# Método Construtor
+# Instancia na memória um objeto da classe Aluno
     def __init__(self, matricula, nome, endereco):
         self.matricula = matricula
         self.nome = nome
         self.endereco = endereco
         self.disciplinas=[]
-       
+
+# Métodos Acessores
+# Retorna por exemplo o valor de
+# um atributo
     def get_matricula(self):
         return self.matricula
     
@@ -18,6 +23,9 @@ class Aluno:
     def get_disciplinas(self):
         return self.disciplinas
 
+# Metodos Modificadores
+# Altera o valor de um atribuo de um Objeto
+
     def set_matricula(self, matricula):
         self.matricula = matricula
         
@@ -27,8 +35,12 @@ class Aluno:
     def set_endereco(self, endereco):
         self.endereco = endereco
     
-    def toString(self):         
+    def toString(self):
         return (str(self.matricula) + " " + self.nome + " " + self.endereco)
+
+    def printSelf(self):
+        print("Valor de self=" + str(self))
+        return
     
     
     
--- aula01/teste.py
+++ aula01/teste.py
@@ -1,8 +1,19 @@
 
 from aluno import Aluno 
 
+# Construindo dois objetos da classe Aluno
+# com os seus atributos
 a = Aluno(100, "Joana", "Av. 13 de Maio 2081")
 b = Aluno(200, "João Pedro", "Av. João Pessoa 1200")
+
+print("Matricula= " + str(a.get_matricula()))
+print("Nome= " + str(a.get_nome()))
+
+a.set_matricula(150)
+a.set_nome("Maria Joana")
+
+print("Matricula= " + str(a.get_matricula()))
+print("Nome= " + str(a.get_nome()))
 
 print(a)
 print(b)
@@ -10,4 +21,5 @@ a.printSelf()
 print(a.toString())
 b.printSelf()
 print(b.toString())
+
 
