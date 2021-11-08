4b936e16b309a717972a3943fca5ec3dbaf97f91
Ricardo Taveira <ricdtaveira@gmail.com>
2021-11-08 17:46:01 -0300

    Alteração em 08-11-2021

--- sqlalchemy01/user.py
+++ sqlalchemy01/user.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./teste.db'
 db = SQLAlchemy(app)
 
-## Defiição de uma classe User que será mapeada para uma tabela User
+## Definição de uma classe User que será mapeada para uma tabela User
 class User(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
